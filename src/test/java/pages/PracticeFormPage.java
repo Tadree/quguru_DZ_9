fcb0916940feaa855de7c1daa8426e5902f8db74
Nikitina Elena <Elen.Nikitina6@x5.ru>
2021-09-09 21:09:17 +0300

    added FormTest

---
+++ src/test/java/pages/PracticeFormPage.java
@@ -0,0 +1,93 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import components.Calendar;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static java.lang.String.format;
+
+public class PracticeFormPage {
+
+    private SelenideElement modal = $("[role=dialog]");
+
+    private Calendar calendar = new Calendar();
+
+    public void openPage (){
+        open("/automation-practice-form");
+    }
+
+    public PracticeFormPage typeFirstName(String firstName) {
+        $("#firstName").setValue(firstName);
+        return this;
+    }
+
+    public PracticeFormPage typeLastName(String lastName) {
+        $("#lastName").setValue(lastName);
+        return this;
+    }
+
+    public PracticeFormPage typeEmail(String email) {
+        $("#userEmail").setValue(email);
+        return this;
+    }
+
+    public PracticeFormPage selectGender(String gender) {
+        $("#genterWrapper").$(byText(gender)).click();
+        return this;
+    }
+
+    public PracticeFormPage typePhone(String phone) {
+        $("#userNumber").setValue(phone);
+        return this;
+    }
+
+    public PracticeFormPage setDateOfBirth(String day, String month, String year) {
+        calendar.setDate(day, month, year);
+        return this;
+    }
+
+    public PracticeFormPage setSubject(String subject){
+        $("#subjectsInput").setValue(subject).pressEnter();
+        return this;
+    }
+
+    public PracticeFormPage setHobby(String hobby){
+        $("#hobbiesWrapper").$(byText(hobby)).click();
+        return this;
+    }
+
+    public PracticeFormPage uploadPicture(String pictureName){
+        $("#uploadPicture").uploadFile(new File("src/test/resources/" + pictureName));
+        return this;
+    }
+
+    public PracticeFormPage setCurrentAddress(String currentAddress){
+        $("#currentAddress").setValue(currentAddress);
+        return this;
+    }
+
+    public PracticeFormPage setState(String state){
+        $("#react-select-3-input").setValue(state).pressEnter();
+        return this;
+    }
+
+    public PracticeFormPage setCity(String city){
+        $("#react-select-4-input").setValue(city).pressEnter();
+        return this;
+    }
+
+    public PracticeFormPage submit(){
+        $("#submit").scrollTo().click();
+        return this;
+    }
+
+    public PracticeFormPage checkResultsValue(String value) {
+        modal.$(".table-responsive").shouldHave(text(value));
+        return this;
+    }
+}
---
+++ src/test/java/ru/tadree/FormTest.java
@@ -0,0 +1,70 @@
+package ru.tadree;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.PracticeFormPage;
+
+
+public class FormTest {
+
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+    String gender = faker.demographic().sex();
+    String phone = faker.numerify("##########");
+    String day = "1";
+    String month = "January";
+    String year = "2000";
+    String subject = "Biology";
+    String hobby = "Music";
+    String picture = "DZ2.png";
+    String currentAddress = faker.address().fullAddress();
+    String state = "Uttar Pradesh";
+    String city = "Agra";
+
+
+    PracticeFormPage practiceFormPage = new PracticeFormPage();
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void positiveFillTest () {
+
+        practiceFormPage.openPage();
+
+        //заполнение формы
+        practiceFormPage.typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeEmail(email)
+                .selectGender(gender)
+                .typePhone(phone)
+                .setDateOfBirth("01","January", "2000")
+                .setSubject(subject)
+                .setHobby(hobby)
+                .uploadPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .setState("Uttar Pradesh")
+                .setCity("Agra")
+                .submit();
+
+        //проверка введенных данных
+        practiceFormPage.checkResultsValue(firstName + " " + lastName)
+                .checkResultsValue(email)
+                .checkResultsValue(gender)
+                .checkResultsValue(phone)
+                .checkResultsValue(day + " " + month + "," + year)
+                .checkResultsValue(subject)
+                .checkResultsValue(hobby)
+                .checkResultsValue(picture)
+                .checkResultsValue(currentAddress)
+                .checkResultsValue(state + " " + city);
+
+    }
+}
