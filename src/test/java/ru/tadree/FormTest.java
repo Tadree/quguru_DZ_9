68fadef852bbe7b789712bf3480c0fa3b4da4a28
Nikitina Elena <Elen.Nikitina6@x5.ru>
2021-09-09 22:19:39 +0300

    added capabilities

--- src/test/java/ru/tadree/FormTest.java
+++ src/test/java/ru/tadree/FormTest.java
@@ -1,12 +1,15 @@
 package ru.tadree;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PracticeFormPage;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 
 
 import static io.qameta.allure.Allure.step;
@@ -36,6 +39,10 @@ public class FormTest {
 
     @BeforeAll
     static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
